f355938b92bfaffb702a9bafe095b970224f1b0b
yhando24 <youcef.handoura@gmail.com>
2018-11-20 12:38:22 +0100

    fin

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -2,11 +2,12 @@ package zoo;
 
 public class Animal {
 	
+	
+	// attribut en protected pour heritage
 	protected String nom;
 	protected String type;
 	
 	public Animal(String nom, String type) {
-
 		this.nom = nom;
 		this.type = type;
 	}
