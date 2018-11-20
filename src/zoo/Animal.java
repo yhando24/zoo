b95621c3a58666b0e2e6d726eddf4db8c72cb36b
yhando24 <youcef.handoura@gmail.com>
2018-11-20 12:36:59 +0100

    fin tp 6.1

---
+++ src/zoo/Animal.java
@@ -0,0 +1,16 @@
+package zoo;
+
+public class Animal {
+	
+	protected String nom;
+	protected String type;
+	
+	public Animal(String nom, String type) {
+
+		this.nom = nom;
+		this.type = type;
+	}
+	
+	
+	
+}
