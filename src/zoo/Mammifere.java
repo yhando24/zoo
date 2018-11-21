5660e09db6424d4e045968ec46ee10d3c9acc87d
yhando24 <youcef.handoura@gmail.com>
2018-11-21 09:49:12 +0100

    fin

---
+++ src/zoo/Mammifere.java
@@ -0,0 +1,16 @@
+package zoo;
+
+public class Mammifere extends Animal{
+
+	public Mammifere(String nom, String type, String regimeAlimentaire) {
+		super(nom, type, regimeAlimentaire);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+
+		
+}
